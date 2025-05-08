66ce79fff8fa46f42db3b00d540fcb44b12f8e44
Sui10an <go314513an@gmail.com>
2025-05-08 10:04:01 +0900

    5/8-3

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -30,6 +30,14 @@ public class TitleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.touchSupported){//タッチ入力
+            buttons.SetActive(true);
+            Change_Screen+=1;
+        }
+        if(Input.GetMouseButton(0)){//タッチ入力
+            buttons.SetActive(true);
+            Change_Screen+=1;
+        }
         if(Input.GetKey(KeyCode.Return)){//Enterで表示
             buttons.SetActive(true);
             Change_Screen+=1;
