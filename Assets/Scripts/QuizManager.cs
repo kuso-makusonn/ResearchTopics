3e3e6d875f974bb07aff2520df8d3dc14a0fd805
Sui10an <go314513an@gmail.com>
2025-06-05 10:08:49 +0900

    6/5-1

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -14,16 +14,20 @@ public class QuizManager : MonoBehaviour
     [SerializeField] GameObject rootQuiz,quiz,quizStartPanel,ansPanel,difficultySelection;
     TextAsset csvFile;// CSVファイル
     List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+    List<int> quizNumbers = new List<int>();//クイズの問題番号を入れる箱
+    List<int> quizAnsNumber = new List<int>{ 2,3,4,5 };
     int difficulty;//難易度番号1~4　数字が小さいほうが簡単
     int Ans=0;//回答番号
     int q = 1;//問題番号
-    public AudioClip sound1,sound2;//音1,音2
+    int ans1,ans2,ans3,ans4 = 0;
+    public AudioClip sound1, sound2;//音1,音2
     AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();//音のComponentを取得
-        csvFile = Resources.Load("課題研究-クイズ") as TextAsset; // Resouces下のCSV読み込み
+        //csvFile = Resources.Load("課題研究-クイズ") as TextAsset; // Resouces下のCSV読み込み,クイズを追加できるモノがある
+        csvFile = Resources.Load("課題研究-クイズ_1") as TextAsset; // Resouces下のCSV読み込み,クイズのみ
         StringReader reader = new StringReader(csvFile.text);
         // , で分割しつつ一行ずつ読み込み
         // リストに追加していく
@@ -31,6 +35,10 @@ public class QuizManager : MonoBehaviour
         {
             string line = reader.ReadLine(); // 一行ずつ読み込み
             csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+        for (int i = 1; i <= csvDatas.Count; i++)//クイズの問題のリスト
+        {
+            quizNumbers.Add(i);
         }
         difficultySelection.SetActive(true);//難易度選択画面表示
         rootQuiz.SetActive(false);//クイズ画面隠す
@@ -42,30 +50,46 @@ public class QuizManager : MonoBehaviour
         }
     }
 
-    void QASet(){//問いの文章と各選択肢を設定
+    void QASet()
+    {//問いの文章と各選択肢を設定
+        int question = Random.Range(1, quizNumbers.Count);//問題のランダム化
+        q = quizNumbers[question];
+        // Fisher-Yates Shuffleでシャッフル
+        for (int i = 0; i < quizAnsNumber.Count; i++)
+        {
+            int rnd = Random.Range(i, quizAnsNumber.Count);
+            int temp = quizAnsNumber[i];
+            quizAnsNumber[i] = quizAnsNumber[rnd];
+            quizAnsNumber[rnd] = temp;
+        }
+        ans1 = quizAnsNumber[0];
+        ans2 = quizAnsNumber[1];
+        ans3 = quizAnsNumber[2];
+        ans4 = quizAnsNumber[3];
         ansPanel.SetActive(false);
-        quizText.text=csvDatas[q][1];
-        b1Text.text=csvDatas[q][2];
-        b2Text.text=csvDatas[q][3];
-        b3Text.text=csvDatas[q][4];
-        b4Text.text=csvDatas[q][5];
+        quizText.text = csvDatas[q][1];
+        b1Text.text = csvDatas[q][ans1];
+        b2Text.text = csvDatas[q][ans2];
+        b3Text.text = csvDatas[q][ans3];
+        b4Text.text = csvDatas[q][ans4];
+        quizNumbers.RemoveAt(question);//出た問題をかぶらないようにする
     }
 
     //回答ボタン関数
     public void A(){
-        Ans=1;//ボタン1を押した反応
+        Ans=ans1 - 1;//ボタン1を押した反応
         CheckAnswer();
     }
     public void B(){
-        Ans=2;//ボタン1を押した反応
+        Ans=ans2 - 1;//ボタン2を押した反応
         CheckAnswer();
     }
     public void C(){
-        Ans=3;//ボタン1を押した反応
+        Ans=ans3 - 1;//ボタン3を押した反応
         CheckAnswer();
     }
     public void D(){
-        Ans=4;//ボタン1を押した反応
+        Ans=ans4 - 1;//ボタン4を押した反応
         CheckAnswer();
     }
     public void CheckAnswer(){//政党判定
