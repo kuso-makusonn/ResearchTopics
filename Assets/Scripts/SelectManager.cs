2554ae69f6fd0545de2dd5ab3179db6f770098cb
Sui10an <go314513an@gmail.com>
2025-06-05 10:52:57 +0900

    6/5-2

--- Assets/Scripts/SelectManager.cs
+++ Assets/Scripts/SelectManager.cs
@@ -8,21 +8,27 @@ public class SelectManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape)){
-            TitleManager.SceneChanger=true;
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            TitleManager.SceneChanger = true;
             SceneManager.LoadScene("TitleScene");
         }
     }
-    public void Mail(){
+    public void Mail()
+    {
         SceneManager.LoadScene("MailScene");
     }
-    public void Web(){
+    public void Web()
+    {
         SceneManager.LoadScene("WebSimScene");
+    }
+    public void SNS(){
+        SceneManager.LoadScene("SNSSimScene");
     }
 }
