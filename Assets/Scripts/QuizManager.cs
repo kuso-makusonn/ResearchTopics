5170b25a45b2ddda7a210c8b07c15fa56ade012e
Sui10an <go314513an@gmail.com>
2025-06-19 09:28:32 +0900

    問題数の設定の可能化

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -11,21 +11,26 @@ using UnityEngine.WSA;
 
 public class QuizManager : MonoBehaviour
 {
-    [SerializeField]TextMeshProUGUI quizText,b1Text,b2Text,b3Text,b4Text,ansText;//テキスト
-    [SerializeField] GameObject title, rootQuiz,quiz,quizStartPanel,ansPanel,difficultySelection;
+    [SerializeField] TextMeshProUGUI quizText, b1Text, b2Text, b3Text, b4Text, ansText, scoreText;//テキスト
+    [SerializeField] GameObject title, rootQuiz, quiz, quizStartPanel, ansPanel, difficultySelection;
     TextAsset csvFile;// CSVファイル
     List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
     List<int> quizNumbers = new List<int>();//クイズの問題番号を入れる箱
-    List<int> quizAnsNumber = new List<int>{ 2,3,4,5 };
+    List<int> quizAnsNumber = new List<int> { 2, 3, 4, 5 };
     int difficulty;//難易度番号1~4　数字が小さいほうが簡単
-    int Ans=0;//回答番号
+    int quizEndNumder;
+    int Ans = 0;//回答番号
     int q = 1;//問題番号
-    int ans1,ans2,ans3,ans4 = 0;
+    int ans1, ans2, ans3, ans4 = 0;
+    int score = 0;//スコア
+    int quizCount = 0;//問題数
     public AudioClip sound1, sound2;//音1,音2
     AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        score = 0;
+        quizCount = 0;
         audioSource = GetComponent<AudioSource>();//音のComponentを取得
         //csvFile = Resources.Load("課題研究-クイズ") as TextAsset; // Resouces下のCSV読み込み,クイズを追加できるモノがある
         csvFile = Resources.Load("CsvFiles/課題研究-クイズ_1") as TextAsset; // Resouces下のCSV読み込み,クイズのみ
@@ -44,11 +49,14 @@ public class QuizManager : MonoBehaviour
         title.SetActive(true);
         rootQuiz.SetActive(false);//クイズ画面隠す
     }
-    void Update(){
-        if (Input.GetKey(KeyCode.Escape)){
-            TitleManager.SceneChanger=true;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            TitleManager.SceneChanger = true;
             SceneManager.LoadScene("TitleScene");
         }
+        scoreText.text = "スコア：" + score.ToString();
     }
     public void QuizStartButton()
     {
@@ -57,59 +65,73 @@ public class QuizManager : MonoBehaviour
     }
 
     void QASet()
-    {//問いの文章と各選択肢を設定
-        int question = Random.Range(1, quizNumbers.Count);//問題のランダム化
-        q = quizNumbers[question];
-        // Fisher-Yates Shuffleでシャッフル
-        for (int i = 0; i < quizAnsNumber.Count; i++)
+    {
+        if (quizCount < quizEndNumder)
         {
-            int rnd = Random.Range(i, quizAnsNumber.Count);
-            int temp = quizAnsNumber[i];
-            quizAnsNumber[i] = quizAnsNumber[rnd];
-            quizAnsNumber[rnd] = temp;
+            quizCount += 1;//クイズの問題数を1増やす
+            //問いの文章と各選択肢を設定
+            int question = Random.Range(1, quizNumbers.Count);//問題のランダム化
+            q = quizNumbers[question];
+            // Fisher-Yates Shuffleでシャッフル
+            for (int i = 0; i < quizAnsNumber.Count; i++)
+            {
+                int rnd = Random.Range(i, quizAnsNumber.Count);
+                int temp = quizAnsNumber[i];
+                quizAnsNumber[i] = quizAnsNumber[rnd];
+                quizAnsNumber[rnd] = temp;
+            }
+            ans1 = quizAnsNumber[0];
+            ans2 = quizAnsNumber[1];
+            ans3 = quizAnsNumber[2];
+            ans4 = quizAnsNumber[3];
+            ansPanel.SetActive(false);
+            quizText.text = csvDatas[q][1];
+            b1Text.text = csvDatas[q][ans1];
+            b2Text.text = csvDatas[q][ans2];
+            b3Text.text = csvDatas[q][ans3];
+            b4Text.text = csvDatas[q][ans4];
+            quizNumbers.RemoveAt(question);//出た問題をかぶらないようにする
         }
-        ans1 = quizAnsNumber[0];
-        ans2 = quizAnsNumber[1];
-        ans3 = quizAnsNumber[2];
-        ans4 = quizAnsNumber[3];
-        ansPanel.SetActive(false);
-        quizText.text = csvDatas[q][1];
-        b1Text.text = csvDatas[q][ans1];
-        b2Text.text = csvDatas[q][ans2];
-        b3Text.text = csvDatas[q][ans3];
-        b4Text.text = csvDatas[q][ans4];
-        quizNumbers.RemoveAt(question);//出た問題をかぶらないようにする
     }
 
     //回答ボタン関数
-    public void A(){
-        Ans=ans1 - 1;//ボタン1を押した反応
+    public void A()
+    {
+        Ans = ans1 - 1;//ボタン1を押した反応
         CheckAnswer();
     }
-    public void B(){
-        Ans=ans2 - 1;//ボタン2を押した反応
+    public void B()
+    {
+        Ans = ans2 - 1;//ボタン2を押した反応
         CheckAnswer();
     }
-    public void C(){
-        Ans=ans3 - 1;//ボタン3を押した反応
+    public void C()
+    {
+        Ans = ans3 - 1;//ボタン3を押した反応
         CheckAnswer();
     }
-    public void D(){
-        Ans=ans4 - 1;//ボタン4を押した反応
+    public void D()
+    {
+        Ans = ans4 - 1;//ボタン4を押した反応
         CheckAnswer();
     }
-    public void CheckAnswer(){//政党判定
-        if(Ans==int.Parse(csvDatas[q][6])){
-            ansText.text="正解だよ";
+    public void CheckAnswer()
+    {//政党判定
+        if (Ans == int.Parse(csvDatas[q][6]))
+        {
+            ansText.text = "正解だよ";
             audioSource.PlayOneShot(sound1);//音１を鳴らす
+            score += 100;
         }
-        else{
-            ansText.text="違うよ";
+        else
+        {
+            ansText.text = "違うよ";
             audioSource.PlayOneShot(sound2);//音２を鳴らす
         }
         StartCoroutine(AnsPanels());
     }
-    IEnumerator AnsPanels(){//正解不正解を表示
+    IEnumerator AnsPanels()
+    {//正解不正解を表示
         ansPanel.SetActive(true);//パネルを表示
         yield return new WaitForSeconds(2f);
         ansPanel.SetActive(false);//パネルを非表示
@@ -122,33 +144,40 @@ public class QuizManager : MonoBehaviour
     /// difficultyに難易度1~4 数字が小さいほうが簡単
     /// ToQAで問題へ移行
     /// </summary>
-    public void DfcButtonA(){
+    public void DfcButtonA()
+    {
         difficulty = 1;
         ToQA();
     }
-    public void DfcButtonB(){
+    public void DfcButtonB()
+    {
         difficulty = 2;
         ToQA();
     }
-    public void DfcButtonC(){
+    public void DfcButtonC()
+    {
         difficulty = 3;
         ToQA();
     }
-    public void DfcButtonD(){
+    public void DfcButtonD()
+    {
         difficulty = 4;
         ToQA();
     }
-    void ToQA(){
+    void ToQA()
+    {
         rootQuiz.SetActive(true);//クイズアクティブ
         quiz.SetActive(false);//クイズ画面は隠す（スタート画面出すため）
         difficultySelection.SetActive(false);//難易度選択画面隠す
         StartCoroutine(QuizStart());
     }
-    IEnumerator QuizStart(){
+    IEnumerator QuizStart()
+    {
         quizStartPanel.SetActive(true);
         yield return new WaitForSeconds(1.5f);
         quizStartPanel.SetActive(false);
         quiz.SetActive(true);
         QASet();//クイズ読み込み・表示
     }
+    
 }
