b754b6856df102d06186c67ad532203659c6e67b
Sui10an <go314513an@gmail.com>
2025-11-06 10:09:43 +0900

    メールの追加、メニューの押せないバグ修正

--- Assets/Scripts/Managers/Scene/MailManager.cs
+++ Assets/Scripts/Managers/Scene/MailManager.cs
@@ -37,7 +37,7 @@ public class MailManager : MonoBehaviour
         SetNextSendMailTime();
         NewMail(0);
         isPhishingMailAttacking = true;
-        NewPhishingMail(0);
+        NewPhishingMail(3);
     }
     private void Update()
     {
